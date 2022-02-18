fa49076d10e5546b1db30d8b02e8e98049998b5f
Luiz Henrique <luizhjramos@outlook.com>
2022-02-18 08:04:18 -0300

    feat: setupFilesAfterEnv to jest access dom elements configure

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import App from "./App";
 
 test("sum", () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<App />); 
 
   expect(getByText("Hello World")).toBeTruthy();
 });
